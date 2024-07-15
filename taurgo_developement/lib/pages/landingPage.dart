5cb4c10e74016cea27fbdcb590b3bb8ae3380a5e
Abishan Parameswaran <62343210+RealAbishan@users.noreply.github.com>
2024-07-15 16:39:37 +0530

    Initial Commit With Landing Page

---
+++ taurgo_developement/lib/pages/landingPage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:taurgo_developement/costants/AppColors.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    // var assetsImage = const AssetImage(
+    //   '/assets/Logos/AllGreen.png'); //<- Creates an object that fetches an image.
+    var image = Image.asset('assets/logo/Taurgo Logo.png',
+        height: 250,
+        fit: BoxFit.scaleDown); //<- Creates a widget that displays an image.
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(color: bWhite),
+          child: Center(
+            child: image,
+          ),
+        ),
+      ),
+    );
+  }
+}
