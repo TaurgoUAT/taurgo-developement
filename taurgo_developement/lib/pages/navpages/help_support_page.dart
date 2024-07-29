6bab7539c679419ef93e3d886d634078bdcbdff3
chanka-herath2001 <cb010779@students.apiit.lk>
2024-07-29 07:27:26 +0530

    updated help and support page

--- taurgo_developement/lib/pages/navpages/help_support_page.dart
+++ taurgo_developement/lib/pages/navpages/help_support_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:taurgo_developement/costants/AppColors.dart'; // Make sure this file contains the necessary color constants
-import 'package:url_launcher/url_launcher.dart';
+import 'package:taurgo_developement/costants/AppColors.dart';
+import 'package:taurgo_developement/pages/navpages/capturingPicturedGuidePage.dart';
+import 'package:taurgo_developement/pages/navpages/pictureTipsGuidePage.dart';
+import 'package:taurgo_developement/pages/navpages/ricohThetaZ1GuidePage.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class HelpSupportPage extends StatelessWidget {
   @override
@@ -41,90 +44,20 @@ class HelpSupportPage extends StatelessWidget {
             children: [
               SearchBarSection(),
               SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 16.0), // Adjust the value as needed
-                child: Text(
-                  'FAQs',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+              _buildNavigationButton(context, 'Connecting to Ricoh Theta Z1',
+                  RicohThetaZ1GuidePage()),
               SizedBox(height: 10),
-              ExpansionTile(
-                title: Text(
-                  'I have never produced a virtual tour before, how do I begin?',
-                ),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'The easiest way to get started, simply book a consultation using our online form or by contacting us directly. During the consultation, we’ll discuss your specific needs and goals, as well as provide a demonstration of our services.',
-                    ),
-                  ),
-                ],
-              ),
-              ExpansionTile(
-                title: Text('Virtual tour creation'),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                        'Our expert team will then use the collected data to create an engaging and interactive 360- degree virtual tour. This process typically takes 3-5 business days, depending on the size and complexity of the property.'),
-                  ),
-                ],
-              ),
-              ExpansionTile(
-                title: Text('Review and approval'),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                        'We’ll share the completed virtual tour with you for review and approval. If any adjustments are needed, our team will make the necessary revisions and resubmit the tour for your final approval.'),
-                  ),
-                ],
-              ),
-              ExpansionTile(
-                title: Text('Launch and promotion'),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                        'Once approved, we’ll help you integrate the virtual tour into your existing marketing materials and platforms. You can then share the tour with potential buyers, providing them with a more engaging and informative experience compared to traditional images or videos. To learn more about our process or to schedule a consultation, please use our online form or call us directly. We’re here to help you enhance your listings and attract more leads with minimal effort.'),
-                  ),
-                ],
-              ),
+              _buildNavigationButton(context, 'Capturing Virtual Tour Pictures',
+                  CapturingPicturesGuidePage()),
+              SizedBox(height: 10),
+              _buildNavigationButton(
+                  context,
+                  'Tips for Capturing Best Quality Pictures',
+                  TipsForCapturingBestImagesPage()),
               SizedBox(height: 30),
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'For more information visit our web page',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton.icon(
-                      onPressed: () async {
-                        const url = 'https://taurgo.co.uk/';
-                        if (await canLaunch(url)) {
-                          await launch(url);
-                        } else {
-                          throw 'Could not launch $url';
-                        }
-                      },
-                      icon: Icon(Icons.web),
-                      label: Text('Taurgo.co.uk'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: kPrimaryColor,
-                        foregroundColor: bWhite, // Background color
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              _buildFAQsSection(),
+              SizedBox(height: 30),
+              _buildMoreInfoSection(),
               SizedBox(height: 20),
               Center(
                 child: TextButton(
@@ -146,6 +79,143 @@ class HelpSupportPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildNavigationButton(
+      BuildContext context, String title, Widget page) {
+    return Center(
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => page),
+            );
+          },
+          child: Text(title),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kPrimaryColor,
+            foregroundColor: bWhite,
+            padding: EdgeInsets.symmetric(vertical: 10),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFAQsSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Text(
+            'FAQs',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        ExpansionTile(
+          title: Text(
+            'What is Taurgo and how can it benefit real estate professionals?',
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Taurgo is a virtual tour app designed to help real estate professionals create immersive virtual tours for their property listings. It allows you to capture 360-degree photos, including floor plans, and share them with potential buyers or tenants, enhancing your property marketing efforts.',
+              ),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: Text(
+            'How do I capture virtual tour photos, including floor plans, using Taurgo and a compatible camera?',
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'To capture virtual tour photos, including floor plans, you can connect the Taurgo app with a compatible camera, such as the Ricoh Theta Z1. Using the Taurgo app, you can capture 360-degree images of your property, and if desired, integrate floor plans into the virtual tour.',
+              ),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: Text(
+            'Is it easy to upload, edit, and share photos, including floor plans, with the Taurgo team?',
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Absolutely! Taurgo makes it easy to upload, edit, and share your virtual tour photos, including floor plans, with the Taurgo team. Once you have captured the photos using the app, you can upload them directly and make any necessary edits or enhancements. You can also include floor plans in the virtual tour to provide a comprehensive view of the property.',
+              ),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: Text(
+            'How long does it take for the Taurgo team to process and send back the completed virtual tour package, including floor plans?',
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'The Taurgo team strives to process and send back the completed virtual tour package, including floor plans, within 24 hours. This allows you to quickly incorporate the final photos and floor plans into your property listings, providing potential buyers or tenants with a comprehensive view of the property.',
+              ),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: Text(
+            'What should I do if I encounter any issues or have questions while using Taurgo?',
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'If you encounter any issues or have questions while using Taurgo, the Taurgo team is there to assist you. Simply send an email to info@taurgo.co.uk, and they will be happy to help you resolve any problems or address any inquiries you may have regarding capturing and including floor plans in your virtual tours.',
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMoreInfoSection() {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'For more information visit our web page',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: () async {
+              const url = 'https://taurgo.co.uk';
+              if (await canLaunchUrlString(url)) {
+                await launchUrlString(url);
+              } else {
+                throw 'Could not launch $url';
+              }
+            },
+            icon: Icon(Icons.web),
+            label: Text('Taurgo.co.uk'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              foregroundColor: bWhite,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class SearchBarSection extends StatelessWidget {
@@ -162,18 +232,16 @@ class SearchBarSection extends StatelessWidget {
             decoration: BoxDecoration(
               color: bWhite,
               border: Border.all(
-                color: kSecondaryButtonBorderColor, // Replace with your desired
-                // border color
-                width: 2.0, // Adjust the border width as needed
-              ), // Background color of the search bar
+                color: kSecondaryButtonBorderColor,
+                width: 2.0,
+              ),
               borderRadius: BorderRadius.circular(30.0),
             ),
             child: Row(
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.search,
-                      color: kSecondaryButtonBorderColor), // Search icon
+                  child: Icon(Icons.search, color: kSecondaryButtonBorderColor),
                 ),
                 Expanded(
                   child: TextField(
@@ -186,8 +254,7 @@ class SearchBarSection extends StatelessWidget {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.mic,
-                      color: kSecondaryButtonBorderColor), // Mic icon
+                  child: Icon(Icons.mic, color: kSecondaryButtonBorderColor),
                 ),
               ],
             ),
